d3bf356382c551da2d18c5e88215ba8ecddea2b2
luisgsousaa <luisgsousaa@gmail.com>
2024-11-22 13:26:48 +0000

    Teste

---
+++ src/main/java/com/mycompany/projeto2/poo/Projeto2POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto2.poo;
+
+/**
+ *
+ * @author Admin
+ */
+public class Projeto2POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
